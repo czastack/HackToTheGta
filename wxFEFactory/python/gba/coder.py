df49c06b3c4d1a35ffab7613984ae01cb8f69cfc
Chenzhenan <2464851375@qq.com>
2017-10-23 20:27:02 +0800

    remove other file

--- wxFEFactory/python/gba/coder.py
+++
@@ -1,3 +0,0 @@
-
-def r1(n):
-    return ((n & 0xFF00) >> 8) | ((n & 0xFF) << 8)
--- wxFEFactory/python/modules/character/config.py
+++
@@ -1,5 +0,0 @@
-ADDR_MAP = {
-    "fe6": {"addr": 0x006076D0, "step": 0x30},
-    "fe7": {"addr": 0x00C4C158, "step": 0x34},
-    "fe8": {"addr": 0x008582BC, "step": 0x34}
-}
